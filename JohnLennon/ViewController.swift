150dafad8e5b5907d87767b153fbf20dae0e0fd6
Peter Sevcik <psevcikbn@gmail.com>
2019-03-22 09:56:01 +0100

    Initial Commit

---
+++ JohnLennon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JohnLennon
+//
+//  Created by Peter on 22/03/2019.
+//  Copyright © 2019 Excellence. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
